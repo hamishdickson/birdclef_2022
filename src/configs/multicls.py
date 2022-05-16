84df318acda7b5ca814fbaa916548eaae3203a8c
Hamish Dickson <hamish.dickson@gmail.com>
2022-05-16 18:09:55 +0100

    hill climbing

--- src/configs/multicls.py
+++ src/configs/multicls.py
@@ -34,7 +34,7 @@ class CFG:
 
     train_bs = 32  # 32
     valid_bs = 32  # 64
-    base_model_name = "tf_efficientnet_b5_ns"
+    base_model_name = "tf_efficientnet_b3_ns"
     starting_weights = None  # "fold-0-b3-779.bin"
     load_up_to_layer = None  # 1
     apex = True
